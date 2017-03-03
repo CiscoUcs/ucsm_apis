31d3ea2f7702616b485b5a40ef1c8aa7933da8b3
vvb <vvb@cisco.com>
2017-03-03 15:51:41 +0530

    support for power on and power off
    Signed-off-by: vvb <vvb@cisco.com>

--- ucsm_apis/server/power.py
+++ ucsm_apis/server/power.py
@@ -14,22 +14,11 @@
 from ..utils.utils import blade_dn_get
 from ..utils.utils import rack_dn_get
 from ucsmsdk.ucsexception import UcsOperationError
+from ucsmsdk.mometa.ls.LsPower import LsPowerConsts
+from ucsmsdk.mometa.ls.LsPower import LsPower
 
 
-def server_power_on(handle, chassis_id=None, balde_id=None, rack_id=None):
-    pass
-
-
-def server_power_off(handle, chassis_id=None, balde_id=None, rack_id=None):
-    pass
-
-
-def _server_admin_state_set(
-        handle,
-        chassis_id=None,
-        blade_id=None,
-        rack_id=None,
-        state=None):
+def _server_dn_get(chassis_id=None, blade_id=None, rack_id=None):
     if chassis_id and blade_id:
         dn = blade_dn_get(chassis_id, blade_id)
     elif rack_id:
@@ -39,11 +28,70 @@ def _server_admin_state_set(
             "server_admin_state_set: Failed to set power state",
             "Missing mandatory arguments. Specify either of, \
             (chassis_id, blade_id) or rack_id")
+    return dn
 
+
+def _service_profile_power_set(
+        handle,
+        chassis_id=None,
+        blade_id=None,
+        rack_id=None,
+        state=None):
+    dn = _server_dn_get(
+        chassis_id=chassis_id,
+        blade_id=blade_id,
+        rack_id=rack_id)
+    blade_mo = handle.query_dn(dn)
+    if blade_mo is None:
+        UcsOperationError(
+            "server_power_set: Failed to set server power",
+            "sever %s does not exist" % (dn))
+
+    if blade_mo.assigned_to_dn is None:
+        UcsOperationError(
+            "server_power_set: Failed to set server power",
+            "sever %s is not associated to a service profile" % (dn))
+
+    sp_mo = handle.query_dn(blade_mo.assigned_to_dn)
+    LsPower(
+        parent_mo_or_dn=sp_mo,
+        state=state)
+    handle.set_mo(sp_mo)
+    handle.commit()
+
+
+def server_power_on(handle, chassis_id=None, blade_id=None, rack_id=None):
+    _service_profile_power_set(
+        handle=handle,
+        chassis_id=chassis_id,
+        blade_id=blade_id,
+        rack_id=rack_id,
+        state=LsPowerConsts.STATE_UP)
+
+
+def server_power_off(handle, chassis_id=None, blade_id=None, rack_id=None):
+    _service_profile_power_set(
+        handle=handle,
+        chassis_id=chassis_id,
+        blade_id=blade_id,
+        rack_id=rack_id,
+        state=LsPowerConsts.STATE_DOWN)
+
+
+def _server_admin_power_set(
+        handle,
+        chassis_id=None,
+        blade_id=None,
+        rack_id=None,
+        state=None):
+    dn = _server_dn_get(
+        chassis_id=chassis_id,
+        blade_id=blade_id,
+        rack_id=rack_id)
     mo = handle.query_dn(dn)
     if mo is None:
         UcsOperationError(
-            "_server_admin_state_set",
+            "_server_admin_power_set",
             "server %s not found" %
             (dn))
 
@@ -57,7 +105,7 @@ def server_power_cycle_wait(
         chassis_id=None,
         blade_id=None,
         rack_id=None):
-    _server_admin_state_set(
+    _server_admin_power_set(
         handle=handle,
         chassis_id=chassis_id,
         blade_id=blade_id,
@@ -70,7 +118,7 @@ def server_power_cycle_immediate(
         chassis_id=None,
         blade_id=None,
         rack_id=None):
-    _server_admin_state_set(
+    _server_admin_power_set(
         handle=handle,
         chassis_id=chassis_id,
         blade_id=blade_id,
