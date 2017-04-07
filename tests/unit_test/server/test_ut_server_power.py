869112f50b577aa8d7cc5d631c1db875d111951d
ragupta <ragupta-git@users.noreply.github.com>
2017-04-07 10:00:29 +0530

    Added unit tests for server power (#25)
    Signed-off-by: Rahul Gupta <ragupta4@cisco.com>

---
+++ tests/unit_test/server/test_ut_server_power.py
@@ -0,0 +1,98 @@
+from mock import Mock, patch
+from nose.tools import assert_raises
+from nose.tools import assert_equal
+
+from ucsmsdk.ucshandle import UcsHandle
+from ucsmsdk.ucsexception import UcsOperationError
+
+from ucsm_apis.server.power import server_power_on
+
+handle = UcsHandle("1.1.1.1", "admin", "dummy")
+
+
+@patch.object(UcsHandle, 'login')
+def test_power_on_all_None(mock_login):
+    mock_login.return_value = True
+
+    expected_error_message = "server_admin_state_set: Failed to set power "\
+        "state failed, error: Missing mandatory arguments. Specify either "\
+        "of (chassis_id, blade_id) or rack_id"
+    with assert_raises(UcsOperationError) as error:
+        server_power_on(handle)
+    assert_equal(error.exception.message, expected_error_message)
+
+
+@patch.object(UcsHandle, 'login')
+def test_power_on_chassis1_bladeNone(mock_login):
+    mock_login.return_value = True
+
+    expected_error_message = "server_admin_state_set: Failed to set power "\
+        "state failed, error: Missing mandatory arguments. Specify either "\
+        "of (chassis_id, blade_id) or rack_id"
+    with assert_raises(UcsOperationError) as error:
+        server_power_on(handle, chassis_id=1)
+    assert_equal(error.exception.message, expected_error_message)
+
+
+@patch.object(UcsHandle, 'login')
+def test_power_on_chassisNone_blade1(mock_login):
+    mock_login.return_value = True
+
+    expected_error_message = "server_admin_state_set: Failed to set power "\
+        "state failed, error: Missing mandatory arguments. Specify either "\
+        "of (chassis_id, blade_id) or rack_id"
+    with assert_raises(UcsOperationError) as error:
+        server_power_on(handle, blade_id=1)
+    assert_equal(error.exception.message, expected_error_message)
+
+
+@patch.object(UcsHandle, 'query_dn')
+@patch.object(UcsHandle, 'login')
+def test_power_on_chassis1_blade1_notexist(mock_login, mock_query_dn):
+    mock_login.return_value = True
+    mock_query_dn.return_value = None
+
+    expected_error_message = "server_power_set: Failed to set server power "\
+        "failed, error: server sys/chassis-1/blade-1 does not exist"
+    with assert_raises(UcsOperationError) as error:
+        server_power_on(handle, chassis_id=1, blade_id=1)
+    assert_equal(error.exception.message, expected_error_message)
+
+
+@patch.object(UcsHandle, 'query_dn')
+@patch.object(UcsHandle, 'login')
+def test_power_on_chassis1_blade1_not_assigned_to_dn(
+        mock_login, mock_query_dn):
+    mock_login.return_value = True
+
+    server_mo = Mock()
+    server_mo.assigned_to_dn = None
+
+    mock_query_dn.return_value = server_mo
+
+    expected_error_message = "server_power_set: Failed to set server power "\
+        "failed, error: server sys/chassis-1/blade-1 is not associated to a "\
+        "service profile"
+    with assert_raises(UcsOperationError) as error:
+        server_power_on(handle, chassis_id=1, blade_id=1)
+    assert_equal(error.exception.message, expected_error_message)
+
+
+@patch.object(UcsHandle, 'commit')
+@patch.object(UcsHandle, 'query_dn')
+@patch.object(UcsHandle, 'login')
+def test_power_on_chassis1_blade1_pass(mock_login, mock_query_dn, mock_commit):
+    from ucsmsdk.mometa.compute.ComputeBlade import ComputeBlade
+    from ucsmsdk.mometa.ls.LsServer import LsServer
+
+    mock_login.return_value = True
+
+    server_mo = Mock()
+    server_mo.assigned_to_dn = "org-root/ls-testsp"
+
+    sp_mo = LsServer("org-root", name="testsp")
+
+    mock_query_dn.side_effect = [server_mo, sp_mo]
+    mock_commit.return_value = None
+
+    assert server_power_on(handle, chassis_id=1, blade_id=1) is None
