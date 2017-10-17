3f5d05df46e568c2e764c35db4ff315e5ca7f905
jeorryb <jeorry@gmail.com>
2017-10-17 11:48:15 -0400

    add unit tests for ippool.py

---
+++ tests/unit_test/network/test_ut_network_ippool.py
@@ -0,0 +1,150 @@
+import pytest
+from ucsm_apis.network import ippool
+from ucsmsdk.ucshandle import UcsHandle
+
+handle = UcsHandle("10.10.10.10", "username", "password")
+
+
+def test_ip_pool_create_success(mocker):
+    mock_login = mocker.patch('ucsmsdk.ucshandle.UcsHandle.login', autospec=True)
+    mock_login.return_value = True
+    mock_query_dn = mocker.patch('ucsmsdk.ucshandle.UcsHandle.query_dn', autospec=True)
+    mock_query_dn.return_value = "org-root"
+    mock_commit = mocker.patch('ucsmsdk.ucshandle.UcsHandle.commit', autospec=True)
+    mock_commit.return_value = None
+    mock_ip_pool = mocker.patch('ucsm_apis.network.ippool.IppoolPool')
+    mock_add_mo = mocker.patch('ucsmsdk.ucshandle.UcsHandle.add_mo', autospec=True)
+    mock_add_mo.return_value = None
+
+    ippool.ip_pool_create(handle, "dummypool")
+
+    mock_query_dn.assert_called_with(handle, "org-root")
+    mock_ip_pool.assert_called_with(descr=None, name='dummypool', parent_mo_or_dn='org-root')
+
+
+def test_ip_pool_create_fail_org_not_exist(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_query_dn = mocker.patch.object(UcsHandle, 'query_dn', autospec=True)
+    mock_query_dn.return_value = None
+
+    with pytest.raises(ippool.UcsOperationError):
+        ippool.ip_pool_create(handle, "dummypool", org_dn="dummy")
+
+
+def test_ip_pool_get_success(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_query_dn = mocker.patch.object(UcsHandle, 'query_dn', autospec=True)
+    mock_query_dn.return_value = "org-root/ip-pool-dummypool"
+
+    ippool.ip_pool_get(handle, "dummypool")
+
+    mock_query_dn.assert_called_with(handle, "org-root/ip-pool-dummypool")
+
+
+def test_ip_pool_get_fail_pool_does_not_exist(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_query_dn = mocker.patch.object(UcsHandle, 'query_dn', autospec=True)
+    mock_query_dn.return_value = None
+
+    with pytest.raises(ippool.UcsOperationError):
+        ippool.ip_pool_get(handle, "nopool")
+
+
+def test_ip_pool_exists_success(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_query_dn = mocker.patch.object(UcsHandle, 'query_dn', autospec=True)
+    mock_check_prop = mocker.patch.object(ippool.IppoolPool, 'check_prop_match', autospec=True)
+    mock_check_prop.return_value = True
+    mock_get = mocker.patch.object(ippool, 'ip_pool_get', autospec=True)
+
+    ippool.ip_pool_exists(handle, "dummypool")
+
+    mock_get.assert_called_with(handle=handle, caller='ip_pool_exists', name= "dummypool", org_dn="org-root")
+
+
+def test_ip_pool_exists_fail_org_does_not_exist(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_get = mocker.patch.object(ippool, 'ip_pool_get', autospec=True)
+    mock_get.side_effect = ippool.UcsOperationError("query_dn", "pool does not exist")
+
+    result = ippool.ip_pool_exists(handle, "dummypool")
+
+    assert result == (False, None)
+
+
+def test_ip_pool_modify_success(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_get = mocker.patch.object(ippool, 'ip_pool_get', autospec=True)
+    managed_object_mock = mocker.Mock()
+    managed_object_mock.set_prop_multiple.return_value = True
+    mock_get.return_value = managed_object_mock
+    mock_set_mo = mocker.patch.object(UcsHandle, 'set_mo', autospec=True)
+    mock_set_mo.return_value = None
+    mock_commit = mocker.patch.object(UcsHandle, 'commit', autospec=True)
+    mock_commit.return_value = None
+
+
+    ippool.ip_pool_modify(handle, "dummypool", descr="This is a new pool")
+
+    mock_get.assert_called_with(handle=handle, name="dummypool", org_dn="org-root", caller="ip_pool_modify")
+    managed_object_mock.set_prop_multiple.assert_called_with(descr="This is a new pool")
+
+
+def test_ip_pool_modify_failure_pool_nonexistent(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_get = mocker.patch.object(ippool, 'ip_pool_get', autospec=True)
+    mock_get.side_effect = ippool.UcsOperationError("query_dn", "pool does not exist")
+
+    with pytest.raises(ippool.UcsOperationError):
+        ippool.ip_pool_modify(handle, "nopool", descr="Pool no aqui")
+
+
+def test_ip_pool_delete_success(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_get = mocker.patch.object(ippool, 'ip_pool_get', autospec=True)
+    mock_remove_mo = mocker.patch.object(UcsHandle, 'remove_mo', autospec=True)
+    mock_commit = mocker.patch.object(UcsHandle, 'commit', autospec=True)
+    mock_commit.return_value = None
+
+    ippool.ip_pool_delete(handle, "dummypool")
+
+    mock_get.assert_called_with(handle=handle, name="dummypool", org_dn="org-root", caller="ip_pool_delete")
+    mock_remove_mo.assert_called()
+
+
+def test_ip_pool_delete_failure_pool_nonexistent(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_get = mocker.patch.object(ippool, 'ip_pool_get', autospec=True)
+    mock_get.side_effect = ippool.UcsOperationError("query_dn", "pool does not exist")
+
+    with pytest.raises(ippool.UcsOperationError):
+        ippool.ip_pool_modify(handle, "nopool", descr="Pool no aqui")
+
+
+def test_ip_block_create_success(mocker):
+    mock_login = mocker.patch.object(UcsHandle, 'login', autospec=True)
+    mock_login.return_value = True
+    mock_query_dn = mocker.patch.object(UcsHandle, 'query_dn', autospec=True)
+    mock_query_dn.return_value = "org-root/ip-pool-dummypool"
+    mock_ip_pool_block = mocker.patch.object(ippool, 'IppoolBlock', autospec=True)
+    mock_add_mo = mocker.patch.object(UcsHandle, 'add_mo', autospec=True)
+    mock_commit = mocker.patch.object(UcsHandle, 'commit', autospec=True)
+
+    ippool.ip_block_create(handle, "dummypool", start_ip="192.168.10.50", end_ip="192.168.10.60",
+                           sm="255.255.255.0", gw="192.168.10.1")
+
+    mock_query_dn.assert_called()
+    mock_ip_pool_block.assert_called_with(parent_mo_or_dn="org-root/ip-pool-dummypool",
+                                          def_gw="192.168.10.1", r_from="192.168.10.50",
+                                          to="192.168.10.60", subnet="255.255.255.0")
+    mock_add_mo.assert_called()
+    mock_commit.assert_called()
